6cb31a266ba2f98ea92085f8fca4dabd0f547dea
xufaj <45463607+xufaj@users.noreply.github.com>
2018-12-15 16:31:22 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 ﻿const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "8";
+var prefix = "1";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);  
